af1a4f004779b59456e9c91c5cad496cfe831a71
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-13 23:35:15 -0500

    ejercicioX: Crear figuras de caballos combinadas en distintas posiciones

--- GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2a.py
+++ GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2a.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+
+caballos1 = knight.negative().join(knight)
+caballos2 = knight.join(knight.negative())
+caballos3 = caballos1.up(caballos2)
+draw(caballos3)
