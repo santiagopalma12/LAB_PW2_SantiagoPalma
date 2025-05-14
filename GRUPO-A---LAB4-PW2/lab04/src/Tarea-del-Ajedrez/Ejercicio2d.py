fddc240842dbfef8769b03df2377ed7c304aed6f
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-13 23:37:18 -0500

    ejercicio4: Crear figura de reina repetida horizontalmente 4 veces

--- GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2d.py
+++ GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2d.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+tablero = square
+tablero = tablero.join(square.negative())
+tablero = tablero.join(tablero)
+tablero = tablero.join(tablero)
+draw(tablero)
