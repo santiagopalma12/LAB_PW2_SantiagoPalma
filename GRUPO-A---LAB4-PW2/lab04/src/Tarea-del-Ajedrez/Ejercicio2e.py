74a762cb84ba8cc2a88af03bd2cb1a946a063124
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-13 23:38:19 -0500

    ejercicio6: Crear tablero de ajedrez con casillas negativas y alternadas

--- GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2e.py
+++ GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2e.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+tablero = square.negative()
+tablero = tablero.join(square)
+tablero = tablero.join(tablero)
+tablero = tablero.join(tablero)
+
+draw(tablero)
