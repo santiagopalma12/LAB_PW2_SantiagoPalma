9b31d1f25fa9c8d58ac024b18cff97e51a150e47
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-13 23:40:54 -0500

    ejercicio2: Crear combinación de caballos con reflejos y unión de piezas

--- GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2b.py
+++ GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+caballo1a = knight.verticalMirror()
+caballo1b = knight.verticalMirror()
+caballos1 = caballo1a.negative().join(caballo1b)
+
+caballos2 = knight.join(knight.negative())
+caballos3 = caballos1.up(caballos2)
+draw(caballos3)
+
