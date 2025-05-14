fd614962723b33a57c12662643205acd46cb25e4
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-05-13 23:38:51 -0500

    ejercicio7: Crear tablero de ajedrez con reflejo vertical y apilamiento

--- GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2f.py
+++ GRUPO-A---LAB4-PW2/lab04/src/Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+tablero = square.negative()
+tablero = tablero.join(square)
+tablero = tablero.join(tablero)
+tablero = tablero.join(tablero)
+tablero = tablero.up(tablero.verticalMirror())
+tablero = tablero.up(tablero)
+draw(tablero)
