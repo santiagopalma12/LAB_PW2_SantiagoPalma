10125e057991e03c420a804b743fa2653a44dbec
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-02 22:37:32 -0500

    Fase 2: Modelo DestinosTuristicos creado y mostrado dinámicamente

--- Lab7_Destinos/destinos_app/models.py
+++ Lab7_Destinos/destinos_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='destinos_imagenes/') # Asegúrate de que MEDIA_ROOT esté configurado
+    precioTour = models.DecimalField(max_digits=10, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombreCiudad
--- Lab7_Destinos/destinos_app/views.py
+++ Lab7_Destinos/destinos_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import DestinosTuristicos # Importar el modelo
 
 def index(request):
-    return render(request, 'index.html')
+    destinos = DestinosTuristicos.objects.all() # Obtener todos los destinos
+    context = {'destinos': destinos}
+    return render(request, 'index.html', context)
