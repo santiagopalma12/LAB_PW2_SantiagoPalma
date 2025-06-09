d10c9bb3ab8c39c359e6a010cef00d892bc4a324
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-08 23:21:54 -0500

    Funcionalidad: Implementa relación Muchos a Muchos entre Libro y Etiqueta

--- lab8/lab8_django/core/admin.py
+++ lab8/lab8_django/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Autor, Libro
+from .models import Autor, Libro, Etiqueta 
 
 admin.site.register(Autor)
 admin.site.register(Libro)
+admin.site.register(Etiqueta)
--- lab8/lab8_django/core/models.py
+++ lab8/lab8_django/core/models.py
@@ -7,10 +7,17 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
 
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 class Libro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
     fecha_publicacion = models.DateField()
+    etiquetas = models.ManyToManyField(Etiqueta, related_name='libros')
 
     def __str__(self):
         return self.titulo
