52c95385c9c149e408b91dbde32d0c9e3d2c6851
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-08 23:33:39 -0500

    Funcionalidad: Implementa envío de emails con backend de consola

--- lab8/lab8_django/core/urls.py
+++ lab8/lab8_django/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import generar_pdf_libro, home
+from .views import generar_pdf_libro, enviar_email_prueba
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='home'),  
     path('libro/<int:pk>/pdf/', generar_pdf_libro, name='libro_pdf'),
+    path('enviar-email/', enviar_email_prueba, name='enviar_email'), # <-- Añadir
 ]
--- lab8/lab8_django/core/views.py
+++ lab8/lab8_django/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from .models import Libro
 from .utils import render_to_pdf
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 def home(request):
     return HttpResponse("Bienvenido a la biblioteca")
@@ -17,3 +18,15 @@ def generar_pdf_libro(request, pk):
         response['Content-Disposition'] = content
         return response
     return HttpResponse("Error al generar el PDF.")
+
+def enviar_email_prueba(request):
+    asunto = '¡Correo de prueba desde Django!'
+    mensaje = 'Si lees esto, el envío de correos funciona.'
+    remitente = 'noreply@mi-biblioteca.com'
+    destinatarios = ['destinatario@ejemplo.com']
+
+    try:
+        send_mail(asunto, mensaje, remitente, destinatarios)
+        return HttpResponse("Correo 'enviado'. Revisa la consola del servidor.")
+    except Exception as e:
+        return HttpResponse(f"Error al enviar el correo: {e}")
