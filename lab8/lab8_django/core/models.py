ed687ab3338adc04c5708ac316d1525d599d44a7
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-06-08 23:21:20 -0500

    Funcionalidad: Implementa relación Uno a Muchos entre Autor y Libro

--- lab8/lab8_django/core/models.py
+++ lab8/lab8_django/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    biografia = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+    fecha_publicacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
